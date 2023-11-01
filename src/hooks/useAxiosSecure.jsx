a441404a52193f282259b9fed4efbc440aee07e6
sumon paul <sumpaul100@gmail.com>
2023-11-01 20:47:26 +0600

    trying to logout

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,5 +1,7 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../providers/AuthProvider";
+import { Navigate } from "react-router-dom";
 
 const useAxios = axios.create({
     baseURL: "http://localhost:5000",
@@ -7,15 +9,17 @@ const useAxios = axios.create({
 })
 
 const useAxiosSecure = () => {
+    const { logOut } = useContext(AuthContext)
     useEffect(() => {
         useAxios.interceptors.request.use(res => {
             return res;
         }, err => {
             if (err.response.status === 401 || err.response.status === 402) {
-                console.log("Log out the user")
+                logOut()
+                    .then(<Navigate to="/login"></Navigate>)
             }
         })
-    }, [])
+    }, [logOut])
     return useAxios
 };
 
